416e306fa3c95fd13aff623b56fe024ab0694f08
Anastasia-Makarova <140186425+Anastasia-Makarova@users.noreply.github.com>
2024-02-17 14:11:00 +0100

    Query for name surname, email + Pydantic validation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 
+
 from db import get_db
 from models import Contact
 from schemas import ContactSchema, ContactResponse
@@ -16,64 +17,100 @@ def root():
     return {"message": "Hello from HW 11"}
 
 
-@app.get('/contacts', response_model=list[ContactResponse])
+@app.get("/contacts", response_model=list[ContactResponse])
 async def get_contacts(db: Session = Depends(get_db)):
     contacts = db.query(Contact).all()
     return contacts
 
 
-@app.get('/contacts/{contact_id}', response_model=ContactResponse, tags=['search_by_parameter'])
+@app.get("/contacts/{contact_id}", response_model=ContactResponse, tags=["search_by_parameter"])
 async def get_contact_by_id(contact_id: int = Path(ge=1), db: Session = Depends(get_db)):
     contact = db.query(Contact).filter_by(id=contact_id).first()
     if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not Found')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
     return contact
 
 
-@app.post('/contacts', response_model=ContactResponse)
+@app.get("/name", response_model=list[ContactResponse], tags=["search_by_parameter"])
+async def get_contact_by_name(contact_name: str, db: Session = Depends(get_db)):
+    contact = db.query(Contact).filter_by(name=contact_name).all()
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    return contact
+
+
+@app.get("/surmane", response_model=list[ContactResponse], tags=["search_by_parameter"])
+async def get_contact_by_surname(contact_surname: str, db: Session = Depends(get_db)):
+    contact = db.query(Contact).filter_by(surname=contact_surname).all()
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    return contact
+
+
+@app.get("/email", response_model=ContactResponse, tags=["search_by_parameter"])
+async def get_contact_by_email(contact_email: str, db: Session = Depends(get_db)):
+    contact = db.query(Contact).filter_by(email=contact_email).first()
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    return contact
+
+
+# @app.get("/birthdays", response_model=list[ContactResponse], tags=["search_by_parameter"])
+# async def get_contact_by_email(start_date: str, db: Session = Depends(get_db)):
+#     contact = db.query(Contact).filter_by(email=contact_email).first()
+#     if contact is None:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+#     return contact
+
+
+@app.post("/contacts", response_model=ContactResponse)
 async def create_contact(body: ContactSchema, db: Session = Depends(get_db)):
     contact = db.query(Contact).filter_by(email=body.email).first()
     if contact:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Contact with this email already exists')
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Contact with this email already exists",
+        )
     contact = Contact(
-        name=body.name, 
-        surname=body.surname, 
-        phone_number=body.phone_number, 
+        name=body.name,
+        surname=body.surname,
+        phone_number=body.phone_number,
         email=body.email,
         birthday=body.birthday,
-        notes=body.notes
-        )
+        notes=body.notes,
+    )
     db.add(contact)
     db.commit()
     return contact
 
 
-@app.put('/contacts/{contact_id}', response_model=ContactResponse)
+@app.put("/contacts/{contact_id}", response_model=ContactResponse)
 async def update_contact(body: ContactSchema, contact_id: int = Path(ge=1), db: Session = Depends(get_db)):
     contact = db.query(Contact).filter_by(id=contact_id).first()
     if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not Found')
-    
-    contact.name=body.name, 
-    contact.surname=body.surname, 
-    contact.phone_number=body.phone_number, 
-    contact.email=body.email,
-    contact.birthday=body.birthday,
-    contact.notes=body.notes
-      
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+
+    contact.name = (body.name,)
+    contact.surname = (body.surname,)
+    contact.phone_number = (body.phone_number,)
+    contact.email = (body.email,)
+    contact.birthday = (body.birthday,)
+    contact.notes = body.notes
+
     db.commit()
     return contact
 
 
-@app.delete('/contacts/{contact_id}', response_model=ContactResponse)
+@app.delete("/contacts/{contact_id}", response_model=ContactResponse)
 async def delete_contact(contact_id: int = Path(ge=1), db: Session = Depends(get_db)):
     contact = db.query(Contact).filter_by(id=contact_id).first()
     if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not Found')
-    
-    db.delete(contact)    
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+
+    db.delete(contact)
     db.commit()
     return contact
+
 
 @app.get("/api/healthchecker")
 def healthchecker(db: Session = Depends(get_db)):
